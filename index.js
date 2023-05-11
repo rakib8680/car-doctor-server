bf9191e8231b513112159f0da31b12eedb6d8135
rakib8680 <rakib.cst.1st@gmail.com>
2023-05-11 20:27:28 +0600

    single data

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -35,6 +35,17 @@ async function run() {
         app.get('/services', async (req, res) => {
             const service = serviceCollection.find()
             const result = await service.toArray();
+            res.send(result);
+        })
+
+        // get single data 
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const options = {
+                projection: { title: 1, price: 1, service_id: 1, img: 1, description: 1, _id:0 }
+            }
+            const result = await serviceCollection.findOne(query, options);
             res.send(result);
         })
 
