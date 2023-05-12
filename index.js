97843ab2aa783845455b986d580c2eb63e2700db
rakib8680 <rakib.cst.1st@gmail.com>
2023-05-12 18:26:03 +0600

    bye

--- index.js
+++ index.js
@@ -52,13 +52,29 @@ async function run() {
 
 
         // bookings 
+        // post 
         app.post('/bookings', async (req, res) => {
             const booking = req.body
             const result = await bookingCollection.insertOne(booking)
             res.send(result);
 
         });
+        // get 
+        app.get('/bookings', async (req, res) => {
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+            const result = await bookingCollection.find(query).toArray()
+            res.send(result)
+        })
 
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await bookingCollection.deleteOne(query)
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection 
         await client.db("admin").command({ ping: 1 });
